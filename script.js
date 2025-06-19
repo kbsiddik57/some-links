9f4675c4f4d848462acbd3e5f203e92c4966528e
kbsiddik57 <96463216+kbsiddik57@users.noreply.github.com>
2025-06-19 20:16:47 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const password = prompt("পাসওয়ার্ড দিন:");
+const correctPassword = ""; // ইচ্ছামতো পাসওয়ার্ড বসাও
+
+if (password !== correctPassword) {
+  alert("ভুল পাসওয়ার্ড!");
+  window.location.href = "about:blank";
+}
+
+function openLink(url) {
+  if (url) window.open(url, '_blank');
+}
+
+    function switchEN() {
+      // ইংরেজি ভার্সনের পেজে রিডাইরেক্ট
+      window.location.href = 'english.html'; // পরিবর্তনযোগ্য
+    }
+
+    function switchBN() {
+      // বাংলা ভার্সনের পেজে রিডাইরেক্ট
+      window.location.href = 'bangla.html'; // পরিবর্তনযোগ্য
+    }
+
+
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+window.addEventListener('scroll', () => {
+  const topBtn = document.querySelector('.top-btn');
+  topBtn.style.display = window.scrollY > 100 ? 'block' : 'none';
+});
